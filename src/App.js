5ac5724e792c2495377cc1000fb161c48a640351
sushiiiii09 <sshreechoudhury@gmail.com>
2023-03-04 00:03:17 +0530

    improving dark mode stage -1

--- src/App.js
+++ src/App.js
@@ -2,15 +2,18 @@ import './App.css';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm'
 import About from './components/About'
+import  React, { useState } from 'react';
 
 function App() {
+  const[mode,setMode]=useState('dark');//says ki dark mode enable hai ki ni 
   return (
     <>
-    <Navbar title="TextUtils" aboutText="About"/>
+    {/* <Navbar title="TextUtils" aboutText="About"/> */}
+    <Navbar title="TextUtils" mode={mode}/>
+
     <div className="container my-4"> 
-    <About/>
       <TextForm heading="Enter your text to analyse"/>
-     
+    <About/>  
 
     </div>
     
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,7 +2,7 @@ import React from 'react'
 import PropTypes from 'prop-types'
 export default function Navbar(props) {
   return (
-    <nav className="navbar navbar-expand-lg bg-light">
+  <nav className={'navbar navbar-expand-lg navbar-${props.mode} bg-${props.mode}'}>
   <div className="container-fluid">
     <a className="navbar-brand" href="/">{props.title}</a>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
